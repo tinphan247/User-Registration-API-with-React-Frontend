b1f8ef32b99b0ba5afb9a794045c0648eacb5ca9
tinphan247 <tinphan111005@gmail.com>
2025-12-02 00:05:06 +0700

    update api url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AlertCircle, CheckCircle2, Loader2, User, Lock, Mail, Home } from 'lucide-react';
 
 const queryClient = new QueryClient();
-const API_BASE_URL = 'https://user-registration-api-t2t8.onrender.com'
+const API_BASE_URL = import.meta.env.PROD 
+  ? 'https://user-registration-api-lrek.onrender.com' // Thay xxxx bằng URL thật từ Render
+  : 'http://localhost:3000';
 interface FormData {
   email: string;
   password: string;
