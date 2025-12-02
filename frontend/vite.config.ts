5c43a28dfde22d5fb978668f38adac6eb7c9a5a9
tinphan247 <tinphan111005@gmail.com>
2025-12-02 16:03:07 +0700

    Prepare for Render deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base : '/User-Registration-API-with-React-Frontend/',
   plugins: [react()],
+  base: '/', // Đơn giản hơn, không cần base path!
 })
