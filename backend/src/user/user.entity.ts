85717743028790129acbdc52b08ed647a2bb9b46
tinphan247 <tinphan111005@gmail.com>
2025-12-11 17:33:26 +0700

    user.entity

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -11,13 +11,10 @@ export class User {
   @Column()
   password: string;
 
-<<<<<<< HEAD
 // THAY ĐỔI: Thêm type: 'varchar' để TypeORM hiểu đây là một chuỗi
   @Column({ nullable: true, type: 'varchar' }) // <--- DÒNG ĐÃ SỬA
   hashedRt: string | null;
 
-=======
->>>>>>> origin/main
   @CreateDateColumn()
   createdAt: Date;
 }
